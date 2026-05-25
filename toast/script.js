f853e38f4127562ce194495d9342c46ab738963d
Luis Calleja <luiscallejaenrique@gmail.com>
2026-05-25 19:37:28 -0300

    feat(toast): create basic functions for DOM manipulation - Add a function to toggle the .show class on the toast element. - Add a function to close the toast when the close button is clicked.

--- toast/script.js
+++ toast/script.js
@@ -2,3 +2,15 @@
 const $button = document.querySelector('.button');
 const $toast = document.querySelector('.toast');
 const $toastClose = document.querySelector('.toast__close');
+
+// functions
+$button.addEventListener('click', () => {
+    $toast.classList.add('show');
+    setTimeout(() => {
+        $toast.classList.remove('show');
+    }, 3000);
+});
+
+$toastClose.addEventListener('click', () => {
+    $toast.classList.remove('show');
+});
