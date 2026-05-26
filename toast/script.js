336730fc76b894d080fcae97f278902194c751fb
Luis Calleja <luiscallejaenrique@gmail.com>
2026-05-26 16:38:21 -0300

    refactor(toast): remove hide class for .toast container - The `.toast` class already has a hidden state with `opacity: 0` and `pointer-events: none`. - The `hide` class was redundant and caused unnecessary complexity in the code. By removing the `hide` class, we can simplify the logic for showing and hiding the toast notifications while maintaining the same functionality. - This change also improves readability and maintainability of the code.

--- toast/script.js
+++ toast/script.js
@@ -8,17 +8,9 @@ $button.addEventListener('click', () => {
     $toast.classList.add('show');
     setTimeout(() => {
         $toast.classList.remove('show');
-        $toast.classList.add('hide');
-        setTimeout(() => {
-            $toast.classList.remove('hide');
-        }, 500);
     }, 3000);
 });
 
 $toastClose.addEventListener('click', () => {
     $toast.classList.remove('show');
-    $toast.classList.add('hide');
-    setTimeout(() => {
-        $toast.classList.remove('hide');
-    }, 500);
 });
